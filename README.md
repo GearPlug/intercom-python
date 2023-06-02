aa2c43f8d86b258565e04ba06bd2399bb624b888
Juan <juankrios15@gmail.com>
2023-06-02 17:05:06 -0500

    added: get and set access token oauth

--- README.md
+++ README.md
@@ -9,6 +9,26 @@ pip install intercom-python
 ```
 ## Usage
 ```python
+# if you have an access token:
 from intercom.client import Client
-
+client = Client(access_token=access_token)
 ```
+```python
+# if you are using Oauth2 to get an access_token:
+from intercom.client import Client
+client = Client(client_id=client_id, client_secret=client_secret)
+```
+To obtain and set an access token:
+1. **Get authorization URL**
+```python
+url = client.authorization_url(redirect_uri)
+```
+2. **Get access token using code**
+```python
+response = client.get_access_token(code)
+```
+3. **Set access token**
+```python
+client.set_token(access_token)
+```
+Check more information about Intercom Oauth: https://developers.intercom.com/building-apps/docs/setting-up-oauth
--- intercom/client.py
+++ intercom/client.py
@@ -1,4 +1,5 @@
 import json
+from urllib.parse import urlencode
 
 import requests
 
@@ -6,5 +7,88 @@ from intercom.exceptions import UnauthorizedError, WrongFormatInputError, Contac
 
 
 class Client(object):
-    def __init__(self):
-        pass
+    URL = "https://api.rd.services/"
+    AUTH_ENDPOINT = "https://app.intercom.com/oauth?"
+    headers = {"Content-Type": "application/json", "Accept": "application/json"}
+
+    def __init__(self, client_id=None, client_secret=None, access_token=None):
+        self.CLIENT_ID = client_id
+        self.CLIENT_SECRET = client_secret
+        if access_token is not None:
+            self.headers.update(Authorization=f"Bearer {access_token}")
+
+    def authorization_url(self, redirect_uri, state=None):
+        params = {"response_type": "code", "client_id": self.CLIENT_ID, "redirect_uri": redirect_uri}
+        if state:
+            params["state"] = state
+        return self.URL + self.AUTH_ENDPOINT + urlencode(params)
+
+    def get_access_token(self, code):
+        body = {"client_id": self.CLIENT_ID, "client_secret": self.CLIENT_SECRET, "code": code}
+        return self.post("auth/eagle/token", data=json.dumps(body))
+
+    def set_token(self, access_token):
+        self.headers.update(Authorization=f"Bearer {access_token}")
+
+    def get_current_user(self):
+        return self.get("me")
+
+    def list_all_admins(self):
+        return self.get("admins")
+
+    def list_all_contacts(self):
+        return self.get("contacts")
+
+    def filter_contacts(self, field, operator, value):
+        """
+        field options: https://developers.intercom.com/intercom-api-reference/reference/searchcontacts  
+        operator options: =, !=, IN, NIN, <, >, ~, !~, ^, $
+        """
+        body = {"field": field, "operator": operator, "value": value}
+        return self.post("auth/eagle/token", data=json.dumps(body))
+
+    def get(self, endpoint, **kwargs):
+        response = self.request("GET", endpoint, **kwargs)
+        return self.parse(response)
+
+    def post(self, endpoint, **kwargs):
+        response = self.request("POST", endpoint, **kwargs)
+        return self.parse(response)
+
+    def delete(self, endpoint, **kwargs):
+        response = self.request("DELETE", endpoint, **kwargs)
+        return self.parse(response)
+
+    def put(self, endpoint, **kwargs):
+        response = self.request("PUT", endpoint, **kwargs)
+        return self.parse(response)
+
+    def patch(self, endpoint, **kwargs):
+        response = self.request("PATCH", endpoint, **kwargs)
+        return self.parse(response)
+
+    def request(self, method, endpoint, **kwargs):
+        return requests.request(method, self.URL + endpoint, headers=self.headers, **kwargs)
+
+    def parse(self, response):
+        status_code = response.status_code
+        if "Content-Type" in response.headers and "application/json" in response.headers["Content-Type"]:
+            try:
+                r = response.json()
+            except ValueError:
+                r = response.text
+        else:
+            r = response.text
+        if status_code == 200:
+            return r
+        if status_code == 204:
+            return None
+        if status_code == 400:
+            raise WrongFormatInputError(r)
+        if status_code == 401:
+            raise UnauthorizedError(r)
+        if status_code == 406:
+            raise ContactsLimitExceededError(r)
+        if status_code == 500:
+            raise Exception
+        return r
